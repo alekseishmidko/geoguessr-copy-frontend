5023fb0b36f52ea3bce976e71f46480466f8e9d5
alekseishmidko <alexeishmidko@gmail.com>
2025-04-21 00:38:29 +0400

    drag-n-drop boxes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { DragNDropBoxes } from "@/features/drag-n-drop-boxes/DragNDropBoxes";
+
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="flex justify-center mt-10">
+      <DragNDropBoxes />
+    </div>
+  );
 }
---
+++ src/features/drag-n-drop-boxes/DragNDropBoxes.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState } from "react";
+import { DndContext, DragEndEvent, closestCenter } from "@dnd-kit/core";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+  arrayMove,
+} from "@dnd-kit/sortable";
+
+import { COLORS } from "@/shared/constants/colors";
+import { SortableCube } from "./components/SortableCube";
+export const DragNDropBoxes = () => {
+  const [items, setItems] = useState<string[]>(COLORS);
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (active.id !== over?.id) {
+      const oldIndex = items.indexOf(String(active.id));
+      const newIndex = items.indexOf(String(over?.id));
+      setItems(arrayMove(items, oldIndex, newIndex));
+    }
+  };
+  return (
+    <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <SortableContext items={items} strategy={verticalListSortingStrategy}>
+        <div className="flex flex-col gap-4 items-center">
+          {items.map((color) => (
+            <SortableCube key={color} id={color} />
+          ))}
+        </div>
+      </SortableContext>
+    </DndContext>
+  );
+};
